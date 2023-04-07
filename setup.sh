db0ec61551161e34802b99fb032d12db5fa0c193
Stavya Datta <stavyad@student.unimelb.edu.au>
2023-04-07 13:23:34 +0530

    Added lazygit to the plugins

--- setup.sh
+++ setup.sh
@@ -35,5 +35,11 @@ apt-get install ripgrep -y
 # Fuzzy file finder
 apt install fzf  -y
 
+# Adding Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit /usr/local/bin
+
 # going back to same repo
 popd
